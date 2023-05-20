bdff13ae496622186dfa8f77dcdfa0342194f089
Masahide Okada <masahideokada1121@gmail.com>
2023-05-20 13:29:35 +0900

    corrected modulate()

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -11,9 +11,9 @@ def modulate(x: Tensor, shift: Tensor, scale: Tensor, latent: Tensor | None = No
     - `x`: normed input tensor with shape (bathes, num channels, embed dim)
     - `shift`: tensor with shape (batches, embed dim)
     - `scale`: tensor with shape (batches, embed dim)
-    - `latent`: autoencoded latent condition with shape (batches, embed dim), optional\n
+    - `latent`: autoencoded latent condition with shape (batches, num channels, embed dim), optional\n
     returns tensor with the same shape as `x`
     """
-    z = 1 + latent.unsqueeze(1) if isinstance(latent, Tensor) else 1
+    z = 1 + latent if isinstance(latent, Tensor) else 1
     h = (1 + scale.unsqueeze(1)) * x + shift.unsqueeze(1)
     return z * h
